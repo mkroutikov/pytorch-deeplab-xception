58673adeb46dac4773e4c285140ff5e24db01b97
Mike Kroutikov <mkroutikov@innodata.com>
2019-05-31 15:43:39 -0400

    playing with params

--- train_scarlet.sh
+++ train_scarlet.sh
@@ -1,2 +1,2 @@
-python train.py --backbone mobilenet --lr 0.01 --workers 0 --epochs 40 --batch-size 3 --gpu-ids 0,1,2,3 --checkname deeplab-mobilenet-scarlet200 --eval-interval 1 --dataset scarlet200 $@
+python train.py --backbone mobilenet --lr 0.01 --workers 4 --epochs 40 --batch-size 10 --gpu-ids 0 --checkname deeplab-mobilenet-scarlet200 --eval-interval 1 --dataset scarlet200 $@
 
